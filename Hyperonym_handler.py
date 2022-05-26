0b4f2088934111bb1dc251430f2b46143488700f
Ivan Ivanov <test@ex.com>
2022-05-26 21:15:27 +0300

    retriving data from neo4j database

--- Hyperonym_handler.py
+++ Hyperonym_handler.py
@@ -9,13 +9,27 @@ class Neo4jClient:
         self.driver = GraphDatabase.driver(uri, auth=(user, password))
         self.session = self.driver.session()
 
+    def is_word_exist(self, the_word):
+        with self.driver.session() as session:
+            result = session.write_transaction(self.__is_word_exist, the_word)
+        if result is not None:
+            return True
+        return False
+
+    @staticmethod
+    def __is_word_exist(tx, the_word):
+        # single return true if we have at least 1 element
+        return tx.run("MATCH (w:Word {name: $the_word}) RETURN w.name", the_word=the_word).single()
+
 class hyperonym_handler:
 
     def __init__(self, model_link = None):
         self.model_link = model_link
         self.morph = pymorphy2.MorphAnalyzer()
+        self.db_client = Neo4jClient('bolt://192.168.85.128:7687', 'neo4j', 'newPassword')
 
     def sentence_parse(self, sentence):
+        print(self.db_client.is_word_exist('мышь 0'))
         splited_sentence = sentence.split(' ')
         normalazed_words = []
         for word in splited_sentence:
