4576f1ce309324842965e5a4264e3f52fcebcd20
Ivan Ivanov <test@ex.com>
2022-05-26 20:02:59 +0300

    trying to import neo4j from ubuntu to winodws10

--- Hyperonym_handler.py
+++ Hyperonym_handler.py
@@ -1,5 +1,13 @@
 import re
 import pymorphy2
+from neo4j import GraphDatabase
+
+class Neo4jClient:
+
+    def __init__(self, uri, user, password):
+        # set  the connection with neo4j
+        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+        self.session = self.driver.session()
 
 class hyperonym_handler:
 
